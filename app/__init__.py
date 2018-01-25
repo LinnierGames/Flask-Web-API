a4b930eae29d90d54008fb756407a8ba7dffe67e
LinnierGames <esericksanc@gmail.com>
2018-01-25 14:06:17 -0800

    added jinja templeting engine

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
-import os
 import json
-from flask import Flask, request
+from flask import Flask, render_template, request
 from app.libraries import Listogram, Stochastic, Markov, RandomWords
 
 # Initialize application
@@ -9,7 +8,10 @@ app = Flask(__name__, static_folder=None)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    num_words = int(request.args.get('words'))
+    rnd_wrds = RandomWords(num_words)
+
+    return render_template("index.html", sentence=str(rnd_wrds))
 
 
 # /generate-sentence?words=5
